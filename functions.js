f6be01350e39d776a89c2ad870ec0d251084aecf
Dalibor Gogic <mail@daliborgogic.com>
2017-12-16 22:40:42 +0100

    correct indent

--- functions.js
+++ functions.js
@@ -1,11 +1,11 @@
 <?php
   add_action('rest_api_init', 'cors', 15);
-    function cors() {
-      remove_filter('rest_pre_serve_request', 'rest_send_cors_headers');
-      add_filter('rest_pre_serve_request', function ($value) {
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET');
-        header('Access-Control-Allow-Credentials: true');
+  function cors() {
+    remove_filter('rest_pre_serve_request', 'rest_send_cors_headers');
+    add_filter('rest_pre_serve_request', function ($value) {
+      header('Access-Control-Allow-Origin: *');
+      header('Access-Control-Allow-Methods: GET');
+      header('Access-Control-Allow-Credentials: true');
       return $value;
     });
   }
